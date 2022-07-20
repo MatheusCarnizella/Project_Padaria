5317600936396f2f89497ddb1cd5a99a45926387
MATHEUS CARNIZELLA <matheus.cne12@gmail.com>
2022-07-20 00:24:51 -0300

    [Projeto_Padarias] {Matheus Carnizella Nepomuceno} (00:23)

---
+++ Project_Padaria_Code/Project_Padaria/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Project_Padaria.SRC;
+
+namespace Project_Padaria
+{
+    internal class Program
+    {
+        enum MENU {Listagem = 1, Cadastro, Excluir, Sair = 9}
+        static List <IInterface> produtos = new List <IInterface> ();
+        static void Main(string[] args)
+        {
+            
+            bool Saida = false;
+            while (Saida == false)
+            {
+                Console.WriteLine("Escolha 9 para sair");
+                int digite = int.Parse(Console.ReadLine());
+
+                MENU menu = (MENU) digite;
+                switch (menu)
+                {
+                    case MENU.Listagem:
+                        break;
+                    case MENU.Cadastro:
+                        break;
+                    case MENU.Excluir:
+                        break;
+                    case MENU.Sair:
+                        Saida = true;
+                        break;
+
+
+                }
+                Console.Clear();
+            }
+            
+        }
+    }
+}
---
+++ Project_Padaria_Code/Project_Padaria/SRC/Produtos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project_Padaria.SRC;
+
+namespace Project_Padaria.SRC
+{
+    internal class Produtos : IInterface
+    {
+        public string Produto;
+        public int Quantidade;
+        public string Sortido;
+        public string Embalado;
+
+        public Produtos(string Produto, int Quantidade, string Sortido, string Embalado)
+        {
+            this.Produto = Produto;
+            this.Quantidade = Quantidade;
+            this.Sortido = Sortido;
+            this.Embalado = Embalado;
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome do produto: {Produto}");
+            Console.WriteLine($"Quantidade Disponivel: {Quantidade}");
+            Console.WriteLine($"O produto é {Sortido}");
+            Console.WriteLine($"O produto é {Embalado}");
+        }
+    }
+}
